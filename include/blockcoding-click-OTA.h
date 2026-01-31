f8ff12c54a89117dc028b5d0ab23e2f1094764c6
To Learn IT <office@to-learn.it>
2026-01-31 21:38:04 +0100

    Added OTA Library

---
+++ include/blockcoding-click-OTA.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// Wrapper header to expose the OTA library from the monorepo root include path.
+#include "../blockcoding-click-OTA/src/blockcoding-click-OTA.h"
---
+++ include/blockcoding-click-certificate.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// Wrapper header to expose the SSL certificate header from the monorepo root include path.
+#include "../blockcoding.click-SSL/blockcoding-click-certificate.h"
